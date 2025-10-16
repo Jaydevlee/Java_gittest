137cfaceb15153d4f1d15a9d0c05cec7ae154051
Jaydevlee <yousick5@gmail.com>
2025-10-16 12:14:02 +0900

    배열 추가

--- Java_train_chap05_/src/chap05/chap05_1/문자열_비교.java
+++ Java_train_chap05_/src/chap05/chap05_1/문자열_비교.java
@@ -6,24 +6,24 @@ public class 문자열_비교 {
 		String strVar1 = "개발자";
 		String strVar2 = "개발자";
 		
-		if(strVar1 == strVar2) {
+		if(strVar1 == strVar2) { //== -> 주소를 비교
 			System.out.println("strVar1과 strVar2는 참조가 같음");
 		} else {
 			System.out.println("strVar1과 strVar2는 참조가 다름");
 		}
 		
-		if(strVar1.equals(strVar2)) {
+		if(strVar1.equals(strVar2)) { //equals -> 문자열 비교
 			System.out.println("strVar1과 strVar2는 문자열이 같음");
 		}
 		String strVar3 = new String("개발자");
 		String strVar4 = new String("개발자");
 		
-		if(strVar3 == strVar4) {
+		if(strVar3 == strVar4) { //== -> 주소를 비교
 			System.out.println("strVar1과 strVar2는 참조가 같음");
 		} else {
 			System.out.println("strVar1과 strVar2는 참조가 다름");
 		}
-		if(strVar3.equals(strVar4)) {
+		if(strVar3.equals(strVar4)) { //equals -> 문자열 비교
 			System.out.println("strVar3과 strVar4는 문자열이 같음");
 		}
 	}
---
+++ Java_train_chap05_/src/chap05/chap05_1/확인문제5.java
@@ -0,0 +1,17 @@
+package chap05.chap05_1;
+
+public class 확인문제5 {
+
+	public static void main(String[] args) {
+		int var1 = 10;
+		int var2 = 10;
+		String var3 = "AB";
+		String var4 = "AB";
+		String var5 = new String("AB");
+		
+		boolean result1 = (var1 == var2);
+		
+		System.out.println(result1);
+	}
+
+}
