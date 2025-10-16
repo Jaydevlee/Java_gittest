16826da92011c42ccd2f113bad50a88b53c0654a
Jaydevlee <yousick5@gmail.com>
2025-10-16 15:58:44 +0900

    연습문제1

---
+++ Java_train_chap05_/src/chap05/chap05_2객체/연습문제/연습문제1.java
@@ -0,0 +1,20 @@
+package chap05.chap05_2객체.연습문제;
+
+public class 연습문제1 {
+
+	public static void main(String[] args) {
+		//배열 생성
+		int[] scores = {80, 74, 63, 97, 88};
+		int maxScore = scores[0];
+		int sum = 0;
+		for(int i = 0; i < scores.length; i++) {
+			sum += scores[i];
+			if(scores[i] > maxScore) {
+				maxScore = scores[i];
+			}
+		}
+		double avg = (double)sum / scores.length;
+		System.out.println("총점: " + sum + ", 평균 " + avg + ", 최대점수 " + maxScore);
+	}
+
+}
---
+++ Java_train_chap05_/src/chap05/chap05_2객체/연습문제/연습문제2.java
@@ -0,0 +1,13 @@
+package chap05.chap05_2객체.연습문제;
+
+import java.util.Scanner;
+
+public class 연습문제2 {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		
+		int nums [] = new int[5];
+	}
+
+}
