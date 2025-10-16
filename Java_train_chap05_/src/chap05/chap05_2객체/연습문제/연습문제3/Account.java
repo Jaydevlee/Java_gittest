32bb263bbdea228a238e05fa18c827f6e5c27fd2
Jaydevlee <yousick5@gmail.com>
2025-10-16 17:09:12 +0900

    연습문제3

---
+++ Java_train_chap05_/src/chap05/chap05_2객체/연습문제/연습문제3/Account.java
@@ -0,0 +1,42 @@
+package chap05.chap05_2객체.연습문제.연습문제3;
+
+import java.util.Scanner;
+
+public class Account {
+	
+	//필드 부분 객체의 값을 저장
+	//private 외부에서 직접 접근 불가
+	private String ano;
+	private String owner;
+	private int balance;
+	
+	//생성자 부분: 클래스로부터 객체를 생성할 때 호출되어 객체의 초기화를 담당
+	//객체 초기화: 필드를 초기화하거나 메소드를 호출해서 객체를 사용할 준비를 하는 것
+	 Account(String ano,String owner,int balance){//생성자 부분:
+		  this.ano=ano;
+		  this.owner=owner;
+		  this.balance=balance;
+		 }
+
+		 public String getAno() {return ano;}
+		 public void setAno(String ano) {this.ano = ano;}
+		 public String getOwner() {return owner;}
+		 public void setOwner(String owner) {this.owner = owner; }
+		 public int getBalance() {return balance;}
+		 public void setBalance(int balance) {this.balance = balance;}
+		
+	
+	// 계좌생성하기
+	Account createAccount() {
+		Scanner s = new Scanner(System.in);
+		System.out.println("계좌번호: ");
+		ano = s.nextLine();
+		System.out.println("계좌주: ");
+		owner = s.nextLine();
+		System.out.println("초기입금액: ");
+		balance = Integer.parseInt(s.nextLine());
+		return new Account(ano, owner, balance);
+		if()
+	}
+}
+
---
+++ Java_train_chap05_/src/chap05/chap05_2객체/연습문제/연습문제3/BankApplication.java
@@ -0,0 +1,48 @@
+package chap05.chap05_2객체.연습문제.연습문제3;
+
+import java.util.Scanner;
+
+public class BankApplication {
+	 private static Account[] accountArray = new Account[100];
+	 private static Scanner s = new Scanner(System.in);
+	 
+	 public static void main(String[] args) {
+		 // Account 배열에서 ano와 동일한 Account1객체 찾기
+		 for(int i = 0; i < accountArray.length; i++) {
+			 accountArray[i] = new Account(null, null, i);
+		 }
+		 boolean run=true;
+		 while(run) {
+			 System.out.println("---------------------------------------------");
+			 System.out.println("1. 계좌생성 | 2. 계좌목록 | 3. 예금  | 4. 출금 | 5. 종료");
+			 System.out.println("---------------------------------------------");
+	   
+			 System.out.print("선택> ");
+	   
+			 int selectNo=s.nextInt();
+	   
+			 if(selectNo==1) {
+				 createAccount();
+			 }else if(selectNo==2) {
+				 accountList();
+			 }else if(selectNo==3) {
+				 deposit();
+			 }else if(selectNo==4) {
+				 withdraw();
+			 }else if(selectNo==5) {
+				 run = false;
+			 }
+		 }
+		 System.out.println("프로그램 종료");
+	 	}
+	 
+	 // 계좌생성하기
+
+	 // 계좌목록보기
+
+	 // 예금하기
+
+	 // 출금하기
+
+	 
+	}
