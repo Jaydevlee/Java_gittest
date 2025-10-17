f0b6c36e828618ae4987c9aaa485d5dcfcaf1c5d
Jaydevlee <yousick5@gmail.com>
2025-10-17 14:41:22 +0900

    클래스 구조분석

--- Java_train_chap05_/src/chap06/chap6_1/Student.java
+++ Java_train_chap05_/src/chap06/chap6_1/Student.java
@@ -1,5 +1,12 @@
 package chap06.chap6_1;
 
 public class Student {
-
-}
+	String name; //변수x 필드 메소드 밖에서 선언됨
+	
+	Student(){} //생성자 식별방법: 클래스 이름과 동일하며 메소드 형식(리턴타입이 없는 메소드)
+	
+	void study () {
+		String str = "학생은 공부를 합니다."; //변수, 메소드 안에서 선언됨
+		System.out.println();
+	} //java에서 void키워드를 가지는 경우는 메소드 일 때, void: 리턴타입
+}	//변수와 메소드 구분; 구문의 끝을 보자
--- Java_train_chap05_/src/chap06/chap6_1/StudentExample.java
+++ Java_train_chap05_/src/chap06/chap6_1/StudentExample.java
@@ -3,7 +3,12 @@ package chap06.chap6_1;
 public class StudentExample {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Student s1 = new Student(); //인스턴스화를 통해 객체 생성
+		System.out.println("s1변수가 Student 객체를 참조");
+		
+		Student s2 = new Student(); //인스턴스화를 통해 객체 생성
+
+		System.out.println("s2변수가 Student 객체를 참조");
 
 	}
 
