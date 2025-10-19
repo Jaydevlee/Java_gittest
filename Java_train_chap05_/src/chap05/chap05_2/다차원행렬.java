a0feb32cd05058323fd396aaefe53a50fcbba5a2
tyu20 <tyu20@DESKTOP-N9H2KBL>
2025-10-19 21:22:01 +0900

    주석추가

--- Java_train_chap05_/src/chap05/chap05_2/다차원행렬.java
+++ Java_train_chap05_/src/chap05/chap05_2/다차원행렬.java
@@ -8,7 +8,9 @@ public class 다차원행렬 {
 				{83, 92, 96},
 				{78, 83, 93, 87, 88}
 		};
+		//행읙 길의
 		System.out.println(array.length);
+		//열의 길이(각 배열의 길이)
 		System.out.println(array[2].length);
 	}
 
