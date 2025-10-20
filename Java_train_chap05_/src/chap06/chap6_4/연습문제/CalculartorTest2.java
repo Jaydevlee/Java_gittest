93434910c6d8cc79904a1f5ef713485affe274c6
tyu20 <tyu20@DESKTOP-N9H2KBL>
2025-10-20 23:36:36 +0900

    연습문제2 풀이

--- Java_train_chap05_/src/chap06/chap6_4/연습문제/CalculartorTest2.java
+++ Java_train_chap05_/src/chap06/chap6_4/연습문제/CalculartorTest2.java
@@ -5,27 +5,32 @@ import java.util.Scanner;
 public class CalculartorTest2 {
 
 	public static void main(String[] args) {
-		Calculator myCal = new Calculator();
+		Calculator2 myCal = new Calculator2();
 		Scanner s = new Scanner(System.in);
-		System.out.print("숫자 2개와 연산자를 입력: ");
-		int num1 = s.nextInt();
-		String op = s.nextLine();
-		int num2 = s.nextInt();
-		double result = 0.0;
 		
-		if(op.equals("+")) {
-			result = myCal.getAdd(num1, num2);
-		} else if(op.equals("-")) {
-			result = myCal.getSub(num1, num2);
-		} else if(op.equals("*")) {
-			result = myCal.getADiv(num1, num2);
+		System.out.print("숫자 2개와 연산자를 입력: ");
+		String s1 = s.nextLine();
+		String op = s.nextLine();
+		String s2 = s.nextLine();
+		
+		double result = 0.0;
+		//둘 중 하나라도 실수면 double 메소드 호출
+		boolean isDouble = s1.contains(".") || s2.contains(".");
+		
+		if(isDouble) {
+			double n1 = Double.parseDouble(s1);
+			double n2 = Double.parseDouble(s2);
+			result = myCal.cal(n1, n2, op);
 		} else {
-			System.out.println("연산자를 정확히 입력");
+			int n1 = Integer.parseInt(s1);
+			int n2 = Integer.parseInt(s2);
+			result = myCal.cal(n1, n2, op);
 		}
-		if(op.equals("/")) {
-			System.out.printf("연산결과: %.7f %n", result);
-		} else {
+		
+		if(result % 1 == 0) {
 			System.out.printf("연산결과: %.0f %n", result);
+		} else {
+			System.out.printf("연산결과: %.7f %n", result);
 		}
 		
 	}
--- Java_train_chap05_/src/chap06/chap6_4/연습문제/Calculator2.java
+++ Java_train_chap05_/src/chap06/chap6_4/연습문제/Calculator2.java
@@ -1,51 +1,45 @@
 package chap06.chap6_4.연습문제;
 
-import java.util.Scanner;
 
 public class Calculator2 {
-	Scanner scanner = new Scanner(System.in);
-	int num1;
-	int num2;
-	
-	int cal(){
-		System.out.print("첫번째 숫자를 입력: ");
-		num1 = scanner.nextInt();
-		System.out.print("첫번째 숫자를 입력: ");
-		num2 = scanner.nextInt();
-		scanner.nextLine();
-		System.out.print("연산자를 입력: ");
-		String op = scanner.nextLine();
-		int result = 0;
-		if(op.equals("+")) {
-			result = num1 + num2;
-		} else if(op.equals("-")){
-			result = num1 - num2;
-		} else if(op.equals("*")) {
-			result = num1 * num2;
-		} else if(op.equals("/")) {
-			result = num1 / num2;
+	//실수용 오버로딩
+	double cal(double num1, double num2, String op) {
+		switch(op) {
+		case "+":
+			return num1 + num2;
+		case "-":
+			return num1 + num2;
+		case "*":
+			return num1 + num2;
+		case "/":
+			if(num2 == 0) {
+				System.out.println("0으로 나눌 수 없습니다.");
+				return 0.0;
+			}
+			return num1 / num2;
+		default:
+			System.out.println("잘못된 연산자입니다.");
+			return 0.0;
 		}
-		return result;
 	}
-	double clal() {
-		System.out.print("첫번째 숫자를 입력: ");
-		num1 = scanner.nextInt();
-		System.out.print("첫번째 숫자를 입력: ");
-		num2 = scanner.nextInt();
-		scanner.nextLine();
-		System.out.print("연산자를 입력: ");
-		String op = scanner.nextLine();
-		int result = 0;
-		if(op.equals("+")) {
-			result = num1 + num2;
-		} else if(op.equals("-")){
-			result = num1 - num2;
-		} else if(op.equals("*")) {
-			result = num1 * num2;
-		} else if(op.equals("/")) {
-			result = num1 / num2;
+	//정수용 오버로딩
+	int cal(int num1, int num2, String op) {
+		switch(op) {
+		case "+":
+			return num1 + num2;
+		case "-":
+			return num1 + num2;
+		case "*":
+			return num1 + num2;
+		case "/":
+			if(num2 == 0) {
+				System.out.println("0으로 나눌 수 없습니다.")
+				return 0;
+			}
+			return num1 / num2;
+		default:
+			System.out.println("잘못된 연산자입니다.");
+			return 0;
 		}
-		return result;
 	}
-
-	}
+}
