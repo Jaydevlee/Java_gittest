b3e052b952cd87127efe6c5830102c6dea6be4b7
tyu20 <tyu20@DESKTOP-N9H2KBL>
2025-10-20 23:03:18 +0900

    연습문제 풀이

--- Java_train_chap05_/src/chap06/chap6_4/연습문제/CalculartorTest2.java
+++ Java_train_chap05_/src/chap06/chap6_4/연습문제/CalculartorTest2.java
@@ -5,15 +5,29 @@ import java.util.Scanner;
 public class CalculartorTest2 {
 
 	public static void main(String[] args) {
-	
-		Scanner scanner = new Scanner(System.in); // 외부에서 Scanner 생성
-
-        Calculator myCalc = new Calculator(0, 0, scanner); // 초기값은 임시
-        myCalc.cal(); // 사용자 입력 기반 계산 수행
-
-        scanner.close(); // 마지막에 닫기
-		 
-		 
+		Calculator myCal = new Calculator();
+		Scanner s = new Scanner(System.in);
+		System.out.print("숫자 2개와 연산자를 입력: ");
+		int num1 = s.nextInt();
+		String op = s.nextLine();
+		int num2 = s.nextInt();
+		double result = 0.0;
+		
+		if(op.equals("+")) {
+			result = myCal.getAdd(num1, num2);
+		} else if(op.equals("-")) {
+			result = myCal.getSub(num1, num2);
+		} else if(op.equals("*")) {
+			result = myCal.getADiv(num1, num2);
+		} else {
+			System.out.println("연산자를 정확히 입력");
+		}
+		if(op.equals("/")) {
+			System.out.printf("연산결과: %.7f %n", result);
+		} else {
+			System.out.printf("연산결과: %.0f %n", result);
+		}
+		
 	}
 
 }
--- Java_train_chap05_/src/chap06/chap6_4/연습문제/Calculator.java
+++ Java_train_chap05_/src/chap06/chap6_4/연습문제/Calculator.java
@@ -3,64 +3,16 @@ package chap06.chap6_4.연습문제;
 import java.util.Scanner;
 
 public class Calculator {
-	int x;
-	int y;
-
-	Calculator(int x, int y){
-		this.x = x;
-		this.y = y;
+	double getAdd(double num1, double num2) {
+		return num1 + num2;
 	}
-	//getAdd
-	int getAdd() {
-		int sum = x + y;
-		return sum;
+	double getSub(double num1, double num2) {
+		return num1 - num2;
 	}
-	int getSub() {
-		int sub = x - y;
-		return sub;
+	double getMul(double num1, double num2) {
+		return num1 * num2;
 	}
-	int getMul() {
-		int mul = x * y;
-		return mul;
-	}
-	double getDiv() {
-		double div = (double) x / y;
-		return div;
-	}
-	
-	void cal() {
-		Scanner scanner = new Scanner(System.in);
-		System.out.print("첫 번째 숫자 입력: ");
-		int num1 = scanner.nextInt();
-		System.out.print("두 번째 숫자 입력: ");
-		int num2 = scanner.nextInt();
-		scanner.nextLine();
-		System.out.print("연산자 입력: ");
-		String op = scanner.nextLine();
-		//입력값으로 새로운 객체 생성
-		Calculator newCalc = new Calculator(num1, num2);
-		
-		switch(op) {
-		case "+":
-			System.out.println("결과: " + newCalc.getAdd());
-			break;
-		case "-":
-			System.out.println("결과: " + newCalc.getSub());
-			break;
-		case "*":
-			System.out.println("결과: " + newCalc.getMul());
-			break;
-		case "/":
-			if(num2 != 0) {
-				System.out.println("결과: " + newCalc.getDiv());
-			} else {
-				System.out.println("결과: 무한대");
-			}
-			break;
-		default:
-			System.out.println("잘 못된 연산자 입니다.");
-		
-		}
-		scanner.close();
+	double getADiv(double num1, double num2) {
+		return num1 / num2;
 	}
 }
