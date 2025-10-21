ccc406ac22723b8933d76a5a59a646b304e8c431
Jaydevlee <yousick5@gmail.com>
2025-10-21 10:17:23 +0900

    .

--- Java_train_chap05_/src/chap06/chap6_5/SingletonExample.java
+++ Java_train_chap05_/src/chap06/chap6_5/SingletonExample.java
@@ -12,7 +12,9 @@ public class SingletonExample {
 		Singleton obj2 = Singleton.getInstance();
 		
 		if(obj1 == obj2) {
-			System.out.println("");
+			System.out.println("같은 Singleton 객체 입니다.");
+		} else {
+			System.out.println("다른 Singleton 객체 입니다.");
 		}
 	}
 
