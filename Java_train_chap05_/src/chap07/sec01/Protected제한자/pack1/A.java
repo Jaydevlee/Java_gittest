e7bc8689ed0f6c2f36748511030b43ee922e0145
Jaydevlee <yousick5@gmail.com>
2025-10-22 11:01:39 +0900

    protecet 접근 제한자

---
+++ Java_train_chap05_/src/chap07/sec01/Protected제한자/pack1/A.java
@@ -0,0 +1,13 @@
+package chap07.sec01.Protected제한자.pack1;
+
+public class A {
+	String field;
+	
+	A() {
+		
+	}
+	
+	void method() {
+		
+	}
+}
---
+++ Java_train_chap05_/src/chap07/sec01/Protected제한자/pack2/C.java
@@ -0,0 +1,11 @@
+package chap07.sec01.Protected제한자.pack2;
+
+import chap07.sec01.Protected제한자.pack1.A;
+
+public class C {
+	public void method() {
+		A.class = new A();
+		a.field = "value";
+		a.method();
+	}
+}
