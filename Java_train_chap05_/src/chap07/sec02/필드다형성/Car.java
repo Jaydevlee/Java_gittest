183ff76b0d1641314fea77df75d794c7e25f6586
Jaydevlee <yousick5@gmail.com>
2025-10-23 15:26:02 +0900

    주석 추가

--- Java_train_chap05_/src/chap07/sec02/필드다형성/Car.java
+++ Java_train_chap05_/src/chap07/sec02/필드다형성/Car.java
@@ -1,6 +1,6 @@
 package chap07.sec02.필드다형성;
 
-public class Car {
+public class Car { //Car클래스와 Tire의 관계 집합관계 (new를 통해 호출), 자동차는 타이어를 가지고 있다. <- 집합관계 / 금호타이어는 타이어다 <- 상속관계
 	//필드
 	Tire frontLeftTire = new Tire("앞왼", 6);
 	Tire frontRightTire = new Tire("앞오", 2);
@@ -11,7 +11,7 @@ public class Car {
 	//메소드
 	int run() {
 		System.out.println("[차가 달립니다.]");
-		if(frontLeftTire.roll() == false) {
+		if(frontLeftTire.roll() == false ) { //사용관계: 다른 클래스의 메소드 호출
 			stop ();
 			return 1;
 		}
--- Java_train_chap05_/src/chap07/sec02/필드다형성/CarExample.java
+++ Java_train_chap05_/src/chap07/sec02/필드다형성/CarExample.java
@@ -13,19 +13,19 @@ public class CarExample {
 			switch(problemLocation) {
 			case 1:
 				System.out.println("앞왼을 HankookTire로 교체");
-				car.frontLeftTire = new HankookTire("앞왼", 80);
+				car.frontLeftTire = new HankookTire("앞왼", 80); //필드의 다형성, hankookTire 클래스의 새로운 주소 생성
 				break;
 			case 2:
 				System.out.println("앞오를 KumhoTire로 교체");
-				car.frontRightTire = new KumhoTire("앞오", 90);
+				car.frontRightTire = new KumhoTire("앞오", 90); //필드의 다형성, kumhoTire 클래스의 새로운 주소 생성
 				break;
 			case 3:
 				System.out.println("뒤왼을 HankookTire로 교체");
-				car.backLeftTire = new HankookTire("뒤왼", 70);
+				car.backLeftTire = new HankookTire("뒤왼", 70); //필드의 다형성
 				break;
 			case 4:
 				System.out.println("뒤오를 HankookTire로 교체");
-				car.backRightTire = new HankookTire("뒤오", 50);
+				car.backRightTire = new KumhoTire("뒤오", 50); //필드의 다형성
 				break;
 			}
 			System.out.println("-------------------");
