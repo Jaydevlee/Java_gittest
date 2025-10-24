b5db8be68a203dd3939313aa93abb9a085bf4d75
Jaydevlee <yousick5@gmail.com>
2025-10-24 16:41:31 +0900

    주석 추가

--- Java_train_chap05_/src/chap08/sec01/다중인터페이스구현/SmartTelevisionExample.java
+++ Java_train_chap05_/src/chap08/sec01/다중인터페이스구현/SmartTelevisionExample.java
@@ -5,6 +5,7 @@ public class SmartTelevisionExample {
 	public static void main(String[] args) {
 		SmartTelevision tv = new SmartTelevision();
 		
+		//Smartelevision클래스가 RemoteControl, Searchable 클래스를 구현하고 있기 때문에 각 타입의 변수와 연결할 수 있다. 
 		RemoteControl rc = tv;
 		Searchable searchable = tv;
 
