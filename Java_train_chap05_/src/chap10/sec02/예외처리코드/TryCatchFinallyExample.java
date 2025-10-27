a7b374520c97f97769a3c13e964099d50796e2e2
Jaydevlee <yousick5@gmail.com>
2025-10-27 14:44:06 +0900

    주석 추가

--- Java_train_chap05_/src/chap10/sec02/예외처리코드/TryCatchFinallyExample.java
+++ Java_train_chap05_/src/chap10/sec02/예외처리코드/TryCatchFinallyExample.java
@@ -4,6 +4,8 @@ public class TryCatchFinallyExample {
 
 	public static void main(String[] args) {
 		try {
+			//              컴파일 에러
+			//              일반 예외
 			Class clazz = Class.forName("java.lang.String2");
 		} catch(ClassNotFoundException e) {
 			System.out.println("클래스가 존재하지 않습니다.");
