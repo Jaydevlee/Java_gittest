9019af7176a1c3bfdb742a3c682144bc61c4269d
Jaydevlee <yousick5@gmail.com>
2025-10-27 14:32:04 +0900

    예외 처리 코드

--- Java_train_chap05_/src/chap09/sec01/확인문제/BackgroundCangeListener.java
+++ Java_train_chap05_/src/chap09/sec01/확인문제/BackgroundCangeListener.java
@@ -3,6 +3,6 @@ package chap09.sec01.확인문제;
 public class BackgroundCangeListener implements CheckBox.OnSelectListener {
 	@Override
 	public void onSelect() {
-		
+		System.out.println("배경을 변경합니다");
 	}
 }
