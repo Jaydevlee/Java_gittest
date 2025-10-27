3f519c0529d48a83d2ab35ae11bf896d1b7df386
Jaydevlee <yousick5@gmail.com>
2025-10-27 15:06:35 +0900

    확인문제 풀이

--- Java_train_chap05_/src/chap10/확인문제/Q4/TryCatchFinallyExample.java
+++ Java_train_chap05_/src/chap10/확인문제/Q4/TryCatchFinallyExample.java
@@ -3,7 +3,7 @@ package chap10.확인문제.Q4;
 public class TryCatchFinallyExample {
 
 	public static void main(String[] args) {
-		String[] strArray = {"10", "20"};
+		String[] strArray = {"10", "2a"};
 		int value = 0;
 		for(int i = 0; i <= 2; i++) {
 			try {
